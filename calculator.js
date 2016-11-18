4413655604c119aead9f99d9accfed1f0d777bf5
zakattack9 <zakattack26111@gmail.com>
2016-11-18 09:05:20 -1000

    fix you probs cuhhh

--- calculator.js
+++ calculator.js
@@ -2,23 +2,23 @@ var output ;
 
 function calculator(x, y, z){
   if (x == "add"){
-  output = y + z;
-  console.log(output);
+   output = y + z;
+   console.log(output);
   }
   else if (x == "subtract"){
-  output = y - z;
-  console.log(output);
+   output = y - z;
+   console.log(output);
   }
   else if (x == "multiply"){
-  output = y * z;
-  console.log(output);
+   output = y * z;
+   console.log(output);
   }
   else if (x == "divide"){
-  output = y / z;
-  console.log(output);
+   output = y / z;
+   console.log(output);
   }
   else{
-  console.log("you failed math, go back to school");
+   console.log("you failed math, go back to school");
   }
 }
 calculator("subtract", 1, 2);
